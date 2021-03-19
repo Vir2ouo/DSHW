5ec1bab752a09915e182c9b4238c29021fdc14dc
Faustine <80964493+Vir2ouo@users.noreply.github.com>
2021-03-19 22:32:11 +0800

    Add files via upload
    資結產物

---
+++ HW0-2.cpp
@@ -0,0 +1,103 @@
+﻿// HW00.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void insertion_sort(int* table, int n, int i)
+{
+	if (i == 0)
+	{
+		*table = n;
+	}
+	else
+	{
+		for (int t = 0; t <= i; t++)
+		{
+			if (*(table + t) > n)
+			{
+				int temp = *(table + t);
+				*(table + t) = n;
+				n = temp;
+
+			}
+			else
+			{
+				*(table + i) = n;
+			}
+		}
+	}
+}
+int delete_arr(int* table, int n, int i)
+{
+	int flag = 0;
+	int t = 0;
+	int index = 0;
+//	printf("%d\n", i);
+	for (t = 0; t <=i; t++)
+	{
+		if (*(table + flag) == n && *(table + t) != n)
+		{
+			*(table + flag) = *(table + t);
+			for (int y = t; y < i - 1; y++)
+			{
+				*(table + y) = *(table + y + 1);
+			}
+			
+			flag++;
+			index++;
+		}
+		else if (*(table + flag) != n && *(table + t) != n)
+		{
+			flag++;
+		}
+
+	}
+	if (*(table + (i - 1)) == n)
+	{
+		index++;
+	}
+//	printf("%d\n", index);
+	return index;
+
+
+}
+void display(int* table, int i)
+{
+	for (int t = 0; t < i; t++)
+	{
+		printf("%d ", *(table + t));
+	}
+}
+int main()
+{
+	char input;
+	int enter, by, del;
+	int box[1001] = { 0 };
+	int i = 0;
+	while (1)
+	{
+		printf("Input or Delete ?");
+		cin >> input;
+		if (input == 'i' || input == 'I')
+		{
+			printf("Input an integer?");
+			scanf_s("%d", &enter);
+			insertion_sort(&box[0], enter, i);
+			i++;
+			display(&box[0], i);
+			printf("\n");
+		}
+		else if (input == 'd' || input == 'D')
+		{
+			printf("Delete which integer?");
+			scanf_s("%d", &del);
+			by = delete_arr(&box[0], del, i);
+			i = i - by;
+			display(&box[0], i);
+			printf("\n");
+		}
+	}
+
+
+}
---
+++ HW1-2.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+struct node {
+	int data;
+	struct node* next;
+};
+struct node* head, * rear;
+
+void create_single_list(void)
+{
+	head = (struct node*)malloc(sizeof(struct node));
+	rear = (struct node*)malloc(sizeof(struct node));
+	head->next = NULL;
+	rear->next = NULL;
+}
+
+int empty(void)
+{
+	if (head->next == NULL)
+		return 1;
+	else
+		return 0;
+}
+void insertNode(int number)
+{
+	struct node* new_node,*prev_node, *this_node;
+	new_node = (struct node*)malloc(sizeof(struct node));
+	new_node->data = number;
+	new_node->next = NULL;
+
+	if (empty())
+	{
+		head->next = new_node;
+		rear-> next = new_node;
+		
+	}
+	else
+	{
+		this_node = head->next;
+		if (number < this_node->data)  //插入到串列前端
+		{
+			head->next = new_node;
+			new_node->next = this_node;
+		}
+		else
+		{
+			while (this_node->next != NULL) //插入到串列中間
+			{
+				prev_node = this_node;
+				this_node = this_node->next;
+				if (number < this_node->data)
+				{
+					prev_node->next = new_node;
+					new_node->next = this_node;
+					return;
+				}
+			}
+			this_node->next = new_node; //插入到串列尾端
+			rear->next = new_node;
+		}
+	}
+
+}
+
+void delete_node(int number)
+{
+	struct node* this_node, * prev_node, * temp_node;
+	prev_node = head;
+	this_node = head->next;
+	while (this_node->next != NULL)  //不是最後一 個節點時
+	{
+		if (number == this_node->data)
+		{
+			temp_node = this_node;
+			prev_node->next = this_node->next;
+			this_node = this_node->next;
+			free(temp_node);
+		}
+		else
+		{
+			prev_node = this_node;
+			this_node=this_node->next;
+		}
+	}
+	if (number == this_node->data) //檢查最後一個節點
+	{
+		temp_node = this_node;
+		prev_node->next = NULL;
+		rear->next = prev_node;
+		free(temp_node);
+	}
+}
+
+
+void printList(void)
+{
+	struct node* this_node;
+	if (!empty())
+	{
+		this_node = head->next;
+		while (this_node->next != NULL)
+		{
+			printf("%d ", this_node->data);
+			this_node = this_node->next;
+		}
+		printf("%d\n", this_node->data);
+	}
+	
+}
+int main()
+{
+	char input;
+	int t = 0;
+	int number;
+
+	create_single_list();
+	while (1)
+	{
+		printf("Input or Delete ?");
+		cin >> input;
+		cout << endl;
+		if (input == 'i' || input == 'I')
+		{
+			printf("Input an integer?");
+			cin >> number;
+			insertNode(number);
+			printList();
+			cout << endl;
+			
+		}
+		else if (input == 'd' || input == 'D')
+		{
+			printf("Input an integer?");
+			cin >> number;
+			delete_node(number);
+			printList();
+		}
+	}
+}
+
+
